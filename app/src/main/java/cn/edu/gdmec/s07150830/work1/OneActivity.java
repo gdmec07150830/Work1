d4357f160227a625a4a4236d5b01539ab39e2e74
gdmec07150830 <879195052@qq.com>
2016-09-06 17:19:00 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/gdmec/s07150830/work1/OneActivity.java
@@ -0,0 +1,13 @@
+package cn.edu.gdmec.s07150830.work1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one);
+    }
+}
